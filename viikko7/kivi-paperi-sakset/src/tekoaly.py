751b50460732aa774ff3ff4ad810d2d1f475723c
Erik Sundius <43874589+esundius@users.noreply.github.com>
2025-12-21 19:04:23 +0200

    refactor

--- viikko7/kivi-paperi-sakset/src/tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/tekoaly.py
@@ -1,17 +1,11 @@
 class Tekoaly:
     def __init__(self):
         self._siirto = 0
+        self._siirrot = {0: 'k', 1: 'p', 2: 's'}
 
     def anna_siirto(self):
-        self._siirto = self._siirto + 1
-        self._siirto = self._siirto % 3
-
-        if self._siirto == 0:
-            return "k"
-        elif self._siirto == 1:
-            return "p"
-        else:
-            return "s"
+        self._siirto = (self._siirto + 1) % 3
+        return self._siirrot[self._siirto]
 
     def aseta_siirto(self, siirto):
         # ei tehdä mitään
